8527b4e40d8e9db8c56f6d22d8724e756ddfa8d2
Abdul Muuiz <aanmuiz@gmail.com>
2025-02-24 23:39:18 +0500

    This is  my final commit

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const fs = require("fs");
 const port = 3000;
 
 
 const server = http.createServer((req,res) => {
+    res.writeHead(200, {"content-type" : "text/html"})
+    fs.readFile('index.html', (error,data) => {
+        if(error) {
+            res.writeHead(404)
+            res.write("Error! File not Found")
+        } else {
+            res.write(data)
+        }
+        res.end();
+    })
 
 })
 
